63b85f42b6c45dc2a957d17b3363b60db0403907
MidsummerYU <adorkable0506@gmail.com>
2025-05-18 17:15:32 +0200

    v0.0.11 - RAG model update

--- get_response/call_chat_model.py
+++ get_response/call_chat_model.py
@@ -22,9 +22,9 @@ else:
     
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct-AWQ", torch_dtype="auto", device_map="auto"
+    "Qwen/Qwen2.5-VL-3B-Instruct", torch_dtype="auto", device_map="auto"
 )
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct-AWQ")
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
 
 async def generate_answer (messages, images):
     try:
